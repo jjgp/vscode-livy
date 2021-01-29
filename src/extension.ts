5bc1b6bceba3c91e7901ba7c75fca2d43789c7d3
Jason Prasad <jasongprasad@gmail.com>
2021-01-29 16:20:20 -0500

    Adds prettier integration

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,15 @@
 import { commands, ExtensionContext, window } from 'vscode';
 
 namespace Commands {
-	export const specifyUrl = 'vscode-livy.specifyUrl';
+    export const specifyUrl = 'vscode-livy.specifyUrl';
 }
 
 export function activate(context: ExtensionContext) {
-	context.subscriptions.push(commands.registerCommand(Commands.specifyUrl, () => {
-		window.showInformationMessage("Eventually you'll be able to specify a URL!");
-	}));
+    context.subscriptions.push(
+        commands.registerCommand(Commands.specifyUrl, () => {
+            window.showInformationMessage("Eventually you'll be able to specify a URL!");
+        }),
+    );
 }
 
 export function deactivate() {}
